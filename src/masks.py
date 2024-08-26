674bde5589a0046690d03a006a57d30ce8a0fbc7
Александр Каюдин <sasha.kayudin@yandex.ru>
2024-08-27 01:41:05 +0300

    git commit

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """Функция маскировки номера карты"""
+    if not card_number.isdigit() or len(card_number) != 16:
+        return None
+    else:
+        return f"""{card_number[:4]} {card_number[4:6]}{'*' * 2} {'*' * 4} {card_number[12:]}"""
+
+
+def get_mask_account(account_number: str) -> str | None:
+    """Функция маскировки номера счёта"""
+    if account_number.isdigit() and len(account_number) == 20:
+        return f"""{'*' * 2}{account_number[-4::]}"""
+    else:
+        return None
+
+
+print(get_mask_card_number("1234567891234569"))
+print(get_mask_account("12345678912345678900"))
